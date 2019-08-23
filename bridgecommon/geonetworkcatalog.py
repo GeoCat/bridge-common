b417b254ebb4c340dbb13f2bec22b546b0596b7f
volaya <volayaf@gmail.com>
2019-08-23 10:53:52 +0200

    use mef.import endpoint for publishing
    # Conflicts:
    #	bridgecommon/geonetworkcatalog.py
    #	bridgecommon/meftools.py

--- bridgecommon/geonetworkcatalog.py
+++ bridgecommon/geonetworkcatalog.py
@@ -6,6 +6,9 @@ class GeoNetworkCatalog(MetadataCatalog):
     def api_url(self):
         return self.service_url + "/srv/api/0.1"
 
+    def xml_services_url(self):
+        return self.service_url + "/srv/eng"
+
     def metadata_exists(self, uuid):
         try:
             self.get_metadata(uuid)
@@ -19,15 +22,17 @@ class GeoNetworkCatalog(MetadataCatalog):
 
     def publish_metadata(self, metadata):
         headers = {"accept": "application/json"}
-        url = self.api_url() + "/records?assignToCatalog=true"        
+        self.nam.setTokenInHeader()
+        url = self.xml_services_url() + "/mef.import=?_csrf=" + self.nam.token
         with open(metadata,'rb') as f:
-            self.nam.setTokenInHeader()
-            files = {'file': (metadata, f, 'application/zip', {'Expires': '0'})}        
-            r = self.nam.session.post(url, files=files, headers = headers)            
+            files = {'mefFile': (metadata, f, 'application/zip', {'Expires': '0'})}        
+            r = self.nam.session.post(url, files=files, headers = headers)
+        print (r.text)
         r.raise_for_status()
-        js = r.json()        
+        '''
         if "errors" in r and r['errors']:
             raise Exception(r['errors'])       
+        '''
 
     def delete_metadata(self, uuid):
         #TODO: bucket??
