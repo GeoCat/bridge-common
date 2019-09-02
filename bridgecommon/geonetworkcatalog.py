2519b6fcdf6ac3758e54e097d7f32d040f2e4c49
volaya <volayaf@gmail.com>
2019-09-02 10:39:26 +0200

    correctly close file after writing it

--- bridgecommon/geonetworkcatalog.py
+++ bridgecommon/geonetworkcatalog.py
@@ -23,9 +23,9 @@ class GeoNetworkCatalog(MetadataCatalog):
     def publish_metadata(self, metadata):
         self.nam.setTokenInHeader()
         url = self.xml_services_url() + "/mef.import"
-        files = {'mefFile': open(metadata, "rb")}        
-        r = self.nam.session.post(url, files=files)
-        print (r.text)
+        with open(metadata, "rb") as f:
+            files = {'mefFile': f}
+            r = self.nam.session.post(url, files=files)
         r.raise_for_status()
         '''
         if "errors" in r and r['errors']:
