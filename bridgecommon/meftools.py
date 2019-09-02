b4fbe134deb471d7eb417ef30b3188e895dd35ff
volaya <volayaf@gmail.com>
2019-09-02 10:39:41 +0200

    add layer preview to mef file

--- bridgecommon/meftools.py
+++ bridgecommon/meftools.py
@@ -6,21 +6,22 @@ from xml.dom import minidom
 from datetime import datetime
 from . import log
 
-def createMef(uuid, metadataFilename, mefFilename):
+def createMef(uuid, metadataFilename, mefFilename, thumbnailFilename):
     z = zipfile.ZipFile(mefFilename, "w")    
     z.write(metadataFilename, os.path.join(uuid, "metadata", os.path.basename(metadataFilename)))
-    info = getInfoXmlContent(uuid)
+    z.write(thumbnailFilename, os.path.join(uuid, "public", os.path.basename(thumbnailFilename)))
+    info = getInfoXmlContent(uuid, thumbnailFilename)
     z.writestr(os.path.join(uuid, "info.xml"), info)
     z.close()
     log.logInfo("MEF file written to %s" % mefFilename)
 
-def _addSubElement(parent, tag, value=None):
-    sub = SubElement(parent, tag)
+def _addSubElement(parent, tag, value=None, attrib=None):
+    sub = SubElement(parent, tag, attrib=attrib or {})
     if value is not None:
         sub.text = value
     return sub
 
-def getInfoXmlContent(uuid):
+def getInfoXmlContent(uuid, thumbnailFilename):
     root = Element("info", {"version": "1.1"})
     general = _addSubElement(root, "general")
     d = datetime.now().isoformat()
@@ -33,7 +34,8 @@ def getInfoXmlContent(uuid):
     _addSubElement(general, "isTemplate", "n")
     _addSubElement(root, "categories")
     _addSubElement(root, "privileges")
-    _addSubElement(root, "public")
+    public = _addSubElement(root, "public")
+    _addSubElement(public, "file", attrib = {"name": os.path.basename(thumbnailFilename), "changeDate": d})
     _addSubElement(root, "private")    
     xmlstring = ElementTree.tostring(root, encoding='UTF-8', method='xml').decode()
     dom = minidom.parseString(xmlstring)    
